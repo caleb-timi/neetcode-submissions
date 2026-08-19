c6a57ee596eaeabab6b77be9edf591ed6ab1a4bb
caleb-timi <calebogundiran@gmail.com>
2026-08-19 13:42:15 +0100

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+
+        const groups = {};
+
+        for (const word of strs){
+            const key = [...word].sort().join("");
+
+            if (!groups[key]){
+                groups[key] = [];
+            }
+
+            groups[key].push(word);
+        }
+
+        return Object.values(groups);
+    
+    }
+}
